b20c2b8fe361144b1188aac9b4fa33f8fe3791f8
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-02 10:13:38 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_g55_xpro_freertos
    MH3-50646
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_g55_xpro_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_g55_xpro_freertos/definitions.h
@@ -54,7 +54,7 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/efc/plib_efc.h"
-#include "peripheral/flexcom/twi/plib_flexcom6_twi.h"
+#include "peripheral/flexcom/twi/master/plib_flexcom6_twi_master.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "driver/i2c/drv_i2c.h"
@@ -73,6 +73,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 119996416
 
 // *****************************************************************************
 // *****************************************************************************
@@ -166,22 +169,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* I2C0 Driver Object */
