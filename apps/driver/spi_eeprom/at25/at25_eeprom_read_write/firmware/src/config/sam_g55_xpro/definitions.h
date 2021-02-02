d236a067b1b748801d894fda6ff00bad282eb716
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-02-02 10:21:02 +0530

    [DRV_SPI_EEPROM] Regenerated demo for driver_spi_eeprom_at25_at25_eeprom_read_write_sam_g55_xpro
    MH3-50651
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_g55_xpro/definitions.h
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_g55_xpro/definitions.h
@@ -55,7 +55,7 @@
 #include "peripheral/cmcc/plib_cmcc.h"
 #include "driver/at25/drv_at25.h"
 #include "peripheral/efc/plib_efc.h"
-#include "peripheral/flexcom/spi/plib_flexcom5_spi.h"
+#include "peripheral/flexcom/spi/master/plib_flexcom5_spi_master.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "system/cache/sys_cache.h"
@@ -72,6 +72,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 119996416
 
 // *****************************************************************************
 // *****************************************************************************
@@ -165,22 +168,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* AT25 Driver Object */
